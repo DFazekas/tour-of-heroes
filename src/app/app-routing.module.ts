21fbfde22597838d3dbf1f6aa5e0fc9c7aacf7c6
Devon Fazekas <dfazekas@DPL-C02W54X9J1GG.local>
2019-01-14 10:13:22 -0500

    Fix RoutingModule. Refactor App template.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,13 @@ import { HeroesComponent } from "./heroes/heroes.component";
 import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
 
-// TODO: FIX
-// @NgModule({
-//   exports: [RouterModule],
-//   imports: [
-//     // Initialize router and start listening for browser location changes.
-//     RouterModule.forRoot(routes)
-//   ]
-// })
-
 const routes: Routes = [{ path: "heroes", component: HeroesComponent }];
 
+@NgModule({
+  exports: [RouterModule],
+  imports: [
+    // Initialize router and start listening for browser location changes.
+    RouterModule.forRoot(routes)
+  ]
+})
 export class AppRoutingModule {}
